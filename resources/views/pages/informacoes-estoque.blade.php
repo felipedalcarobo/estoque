85b419a107eb6937b68d0b6416423511555f30a4
Felipe Dal Carobo <felipedalcarobo@ymail.com>
2020-08-10 00:08:36 -0300

    ajuste para direcionar a tela certa no fazer login ou registro

--- resources/views/pages/informacoes-estoque.blade.php
+++ resources/views/pages/informacoes-estoque.blade.php
@@ -6,7 +6,30 @@
         <h6>Estoque</h6>
     </div>
     <div>
+		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalNovoProduto">Adicionar</button>
+	</div>
+	<div class="content-cadastro-cadastrar" >
+		@include('includes.lista-produtos')
+	</div>
+    <div>
     	
     </div>
+    <div class="modal fade" id="modalNovoProduto" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered modal-md" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalCenterTitle">Cadastrar Novo Produto</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>				
+				@include('includes.cadastrar-produto')
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
+
+
+
+
